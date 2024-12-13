8126a42407bccbaa91cbe309258160afb4330254
cyruu <cyruz.mhr09@gmail.com>
2024-12-13 22:40:48 +0545

    more blogs

--- src/app/blogs/[blogid]/page.tsx
+++ src/app/blogs/[blogid]/page.tsx
@@ -8,6 +8,8 @@ import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 import Link from "next/link";
 const SingleBlog = ({ params }: any) => {
   const blogid = params.blogid;
+  const [noOfBlogs, setnoOfBlogs] = useState(3);
+
   const [loading, setLoading] = useState(true);
   function handleCopyText(element: any) {
     return function () {
@@ -62,7 +64,7 @@ const SingleBlog = ({ params }: any) => {
     };
   }, []);
   return (
-    <div className="singleblog mt-[10vh] min-h-[90vh]">
+    <div className="singleblog  mt-[10vh] min-h-[90vh]">
       {loading ? (
         <div className="loading flex items-start justify-center">
           <div className="mt-10 flex flex-col items-center">
@@ -73,100 +75,103 @@ const SingleBlog = ({ params }: any) => {
       ) : (
         ""
       )}
-      <div className="blogcontainer mt-24 w-[90%] mx-auto mb-16 md:w-[75%]">
-        <div className="titledate py-2 sticky top-[10vh] bg-white z-10">
-          <p className="blogtitle text-xl font-bold   md:text-2xl">
-            {singleBlogObject.title}
+      <div className="blog-otherblogs-container relative flex  mt-24 w-[90%] mx-auto mb-16 md:w-[90%]">
+        <div className="blogcontainer w-full md:w-[75%] md:pr-8 md:border-r md:border-gray-200">
+          <div className="titledate py-2 sticky top-[10vh] bg-white z-10">
+            <p className="blogtitle text-xl font-bold   md:text-3xl">
+              {singleBlogObject.title}
+            </p>
+            {singleBlogObject.date ? (
+              <p className="singleblogdate text-xs text-gray-500 mt-1 ">
+                {singleBlogObject.date} ,&nbsp;Cyrus Maharjan
+              </p>
+            ) : (
+              ""
+            )}
+          </div>
+          <p className="firstIntroduction mt-5 text-xs md:text-base">
+            {singleBlogObject.firstIntroduction}
           </p>
-          {singleBlogObject.date ? (
-            <p className="singleblogdate text-xs text-gray-500 mt-1 ">
-              {singleBlogObject.date} ,&nbsp;Cyrus Maharjan
+          {/* <div
+          className={`ytvideo ${singleBlogObject.ytLink ? "" : "displaynone"}`}
+        > */}
+          <div className={` ytvideo my-7 ${true ? "" : "hidden"}`}>
+            <iframe
+              className="mx-auto w-full h-[210px] md:w-[560px]  md:h-[330px]"
+              src={singleBlogObject.ytLink}
+              title="YouTube Video"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          </div>
+          {singleBlogObject.secondIntroduction &&
+          singleBlogObject.secondfeatures ? (
+            <>
+              {/* second intro */}
+              <p className="secondIntroduction mt-5 text-xs md:text-base">
+                {singleBlogObject.secondIntroduction}
+              </p>
+              {/* second features */}
+              <ul className="my-5">
+                {singleBlogObject.secondfeatures.map(
+                  (feature: String, i: any) => {
+                    return (
+                      <li key={i} className="text-xs text-gray-500 md:text-sm">
+                        <i
+                          className="ri-circle-fill mr-2"
+                          style={{ fontSize: ".4rem" }}
+                        ></i>
+                        {feature}
+                      </li>
+                    );
+                  }
+                )}
+              </ul>
+            </>
+          ) : (
+            ""
+          )}
+          {singleBlogObject.thirdIntroduction &&
+          singleBlogObject.thirdfeatures ? (
+            <>
+              {/* second intro */}
+              <p className="thirdIntroduction mt-5 text-xs md:text-base">
+                {singleBlogObject.thirdIntroduction}
+              </p>
+              {/* second features */}
+              <ul className="my-5">
+                {singleBlogObject.thirdfeatures.map(
+                  (feature: String, i: any) => {
+                    return (
+                      <li key={i} className="test-xs text-gray-500 md:text-sm">
+                        <i
+                          className="ri-circle-fill mr-2"
+                          style={{ fontSize: ".4rem" }}
+                        ></i>
+                        {feature}
+                      </li>
+                    );
+                  }
+                )}
+              </ul>
+            </>
+          ) : (
+            ""
+          )}
+          {/* for sale */}
+          {singleBlogObject.forsale ? (
+            <p className="text-xs md:text-base">
+              Wanna buy code? Visit cyshop{" "}
+              <Link href={singleBlogObject.forsale} className="text-blue-500">
+                here
+              </Link>
             </p>
           ) : (
             ""
           )}
-        </div>
-        <p className="firstIntroduction mt-5 text-xs md:text-sm">
-          {singleBlogObject.firstIntroduction}
-        </p>
-        {/* <div
-          className={`ytvideo ${singleBlogObject.ytLink ? "" : "displaynone"}`}
-        > */}
-        <div className={` ytvideo my-7 ${true ? "" : "hidden"}`}>
-          <iframe
-            className="mx-auto w-full h-[210px] md:w-[560px]  md:h-[330px]"
-            src={singleBlogObject.ytLink}
-            title="YouTube Video"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerPolicy="strict-origin-when-cross-origin"
-            allowFullScreen
-          ></iframe>
-        </div>
-        {singleBlogObject.secondIntroduction &&
-        singleBlogObject.secondfeatures ? (
-          <>
-            {/* second intro */}
-            <p className="secondIntroduction mt-5 text-xs md:text-sm">
-              {singleBlogObject.secondIntroduction}
-            </p>
-            {/* second features */}
-            <ul className="mt-2 mb-5">
-              {singleBlogObject.secondfeatures.map(
-                (feature: String, i: any) => {
-                  return (
-                    <li key={i} className="text-xs md:text-sm">
-                      <i
-                        className="ri-circle-fill mr-2"
-                        style={{ fontSize: ".4rem" }}
-                      ></i>
-                      {feature}
-                    </li>
-                  );
-                }
-              )}
-            </ul>
-          </>
-        ) : (
-          ""
-        )}
-        {singleBlogObject.thirdIntroduction &&
-        singleBlogObject.thirdfeatures ? (
-          <>
-            {/* second intro */}
-            <p className="thirdIntroduction mt-5 text-xs md:text-sm">
-              {singleBlogObject.thirdIntroduction}
-            </p>
-            {/* second features */}
-            <ul className="mt-2 mb-5">
-              {singleBlogObject.thirdfeatures.map((feature: String, i: any) => {
-                return (
-                  <li key={i} className="test-xs md:text-sm">
-                    <i
-                      className="ri-circle-fill mr-2"
-                      style={{ fontSize: ".4rem" }}
-                    ></i>
-                    {feature}
-                  </li>
-                );
-              })}
-            </ul>
-          </>
-        ) : (
-          ""
-        )}
-        {/* for sale */}
-        {singleBlogObject.forsale ? (
-          <p>
-            Wanna buy code? Visit cyshop{" "}
-            <Link href={singleBlogObject.forsale} className="text-blue-500">
-              here
-            </Link>
-          </p>
-        ) : (
-          ""
-        )}
-        {/* if no yt display thubnail */}
-        {/* <div
+          {/* if no yt display thubnail */}
+          {/* <div
           className={`thumbnail w-full md:w-[315px] ${
             singleBlogObject.ytLink ? "hidden" : ""
           }`}
@@ -174,99 +179,137 @@ const SingleBlog = ({ params }: any) => {
 
           <Image src={singleBlogObject.thumbnail} alt="thumbnail" />
         </div> */}
-        {/* index.html */}
-        <div
-          className={`indexhtmlcontainer relative ${
-            singleBlogObject.indexHtml ? "" : "hidden"
-          }`}
-        >
-          <p className="mb-3 text-xs md:text-sm">
-            {/* <p className={`indexhtml ${fa ? "" : "hidden"} `}> */}
-            Code for <span className="font-bold">index.html</span>
-            <button
-              id="copyhtml"
-              title="copy"
-              className="absolute top-14 right-3 md:right-14"
-            >
-              <i className="ri-clipboard-line text-xl text-white  border border-1-white p-1 rounded-lg"></i>
-              <p
-                id="htmlcopied"
-                className="text-gray-300 text-xs md:text-sm mt-1 opacity-0"
-              >
-                copied
-              </p>
-            </button>
-          </p>
-          <SyntaxHighlighter
-            language="html"
-            id="codeBlockHtml"
-            style={oneDark}
-            className="no-scrollbar text-xs h-[400px] md:text-sm md:h-[500px]"
+          {/* index.html */}
+          <div
+            className={`indexhtmlcontainer relative ${
+              singleBlogObject.indexHtml ? "" : "hidden"
+            }`}
           >
-            {singleBlogObject.indexHtml}
-          </SyntaxHighlighter>
+            <p className="mb-3 text-xs md:text-sm">
+              {/* <p className={`indexhtml ${fa ? "" : "hidden"} `}> */}
+              Code for <span className="font-bold">index.html</span>
+              <button
+                id="copyhtml"
+                title="copy"
+                className="absolute top-14 right-3 md:right-14"
+              >
+                <i className="ri-clipboard-line text-xl text-white  border border-1-white p-1 rounded-lg"></i>
+                <p
+                  id="htmlcopied"
+                  className="text-gray-300 text-xs md:text-sm mt-1 opacity-0"
+                >
+                  copied
+                </p>
+              </button>
+            </p>
+            <SyntaxHighlighter
+              language="html"
+              id="codeBlockHtml"
+              style={oneDark}
+              className="no-scrollbar text-xs h-[400px] md:text-sm md:h-[500px]"
+            >
+              {singleBlogObject.indexHtml}
+            </SyntaxHighlighter>
+          </div>
+          {/* styles.css */}
+          <div
+            className={`stylescsscontainer relative ${
+              singleBlogObject.css ? "" : "hidden"
+            }`}
+          >
+            <p className="mb-3 mt-5 text-xs md:text-sm">
+              Code for <span className="font-bold">styles.css</span>
+              <button
+                id="copycss"
+                title="copy"
+                className="absolute top-14 right-3 md:right-14"
+              >
+                <i className="ri-clipboard-line text-xl text-white  border border-1-white p-1 rounded-lg"></i>
+                <p
+                  id="csscopied"
+                  className="text-gray-300 text-xs md:text-sm mt-1 opacity-0"
+                >
+                  copied
+                </p>
+              </button>
+            </p>
+            <SyntaxHighlighter
+              language="css"
+              id="codeBlockCss"
+              style={oneDark}
+              className="no-scrollbar text-xs h-[400px] md:text-sm md:h-[500px]"
+            >
+              {singleBlogObject.css}
+            </SyntaxHighlighter>
+          </div>
+          {/* script.js */}
+          <div
+            className={`scriptjscontainer relative ${
+              singleBlogObject.js ? "" : "hidden"
+            }`}
+          >
+            <p className="mb-3 mt-5 text-xs md:text-sm">
+              Code for <span className="font-bold">script.js</span>
+              <button
+                id="copyjs"
+                title="copy"
+                className="absolute top-14 right-3 md:right-14"
+              >
+                <i className="ri-clipboard-line text-xl text-white  border border-1-white p-1 rounded-lg"></i>
+                <p
+                  id="jscopied"
+                  className="text-gray-300 text-xs md:text-sm mt-1 opacity-0"
+                >
+                  copied
+                </p>
+              </button>
+            </p>
+            <SyntaxHighlighter
+              language="javascript"
+              id="codeBlockJs"
+              style={oneDark}
+              className="no-scrollbar text-xs h-[400px] md:text-sm md:h-[500px]"
+            >
+              {singleBlogObject.js}
+            </SyntaxHighlighter>
+          </div>
         </div>
-        {/* styles.css */}
-        <div
-          className={`stylescsscontainer relative ${
-            singleBlogObject.css ? "" : "hidden"
-          }`}
-        >
-          <p className="mb-3 mt-5 text-xs md:text-sm">
-            Code for <span className="font-bold">styles.css</span>
-            <button
-              id="copycss"
-              title="copy"
-              className="absolute top-14 right-3 md:right-14"
-            >
-              <i className="ri-clipboard-line text-xl text-white  border border-1-white p-1 rounded-lg"></i>
-              <p
-                id="csscopied"
-                className="text-gray-300 text-xs md:text-sm mt-1 opacity-0"
-              >
-                copied
-              </p>
-            </button>
-          </p>
-          <SyntaxHighlighter
-            language="css"
-            id="codeBlockCss"
-            style={oneDark}
-            className="no-scrollbar text-xs h-[400px] md:text-sm md:h-[500px]"
-          >
-            {singleBlogObject.css}
-          </SyntaxHighlighter>
-        </div>
-        {/* script.js */}
-        <div
-          className={`scriptjscontainer relative ${
-            singleBlogObject.js ? "" : "hidden"
-          }`}
-        >
-          <p className="mb-3 mt-5 text-xs md:text-sm">
-            Code for <span className="font-bold">script.js</span>
-            <button
-              id="copyjs"
-              title="copy"
-              className="absolute top-14 right-3 md:right-14"
-            >
-              <i className="ri-clipboard-line text-xl text-white  border border-1-white p-1 rounded-lg"></i>
-              <p
-                id="jscopied"
-                className="text-gray-300 text-xs md:text-sm mt-1 opacity-0"
-              >
-                copied
-              </p>
-            </button>
-          </p>
-          <SyntaxHighlighter
-            language="javascript"
-            id="codeBlockJs"
-            style={oneDark}
-            className="no-scrollbar text-xs h-[400px] md:text-sm md:h-[500px]"
-          >
-            {singleBlogObject.js}
-          </SyntaxHighlighter>
+        {/* other blog list */}
+        <div className="otherblogslist hidden  w-[25%] h-max md:flex md:flex-col md:items-center">
+          {blogs.map((blog) => {
+            if (blogid != blog.id)
+              return (
+                <Link
+                  href={`/blogs/${blog.id}`}
+                  rel="noopener noreferrer"
+                  className="blogitem mb-0 flex flex-col w-[85%] h-[260px] rounded-2xl overflow-hidden transition-all duration-200 ease-in hover:shadow-md md:mb-4 "
+                  key={blog.id}
+                >
+                  <div className="blogimage p-2 h-full overflow-hidden ">
+                    <Image
+                      src={blog.thumbnail}
+                      alt=""
+                      className="rounded-2xl w-full h-full "
+                    />
+                  </div>
+                  <div className="blogdesc flex flex-col px-3 py-1">
+                    <p className="blogdate w-max text-xs text-gray-400">
+                      {blog.date}
+                    </p>
+                    <p className="blogname font-bold text-md md:text-[.85rem]">
+                      {blog.title}
+                    </p>
+                    {/* 
+                  <div className="blogsmall text-xs mt-3 h-full text-gray-400">
+                    {blog.displayIntro}
+                  </div> */}
+                    <span className="blogbutton text-xs mt-3 mb-3 mr-auto">
+                      Read More <i className="ri-arrow-right-line"></i>
+                    </span>
+                  </div>
+                </Link>
+              );
+          })}
         </div>
       </div>
     </div>
